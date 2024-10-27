6379b3e682c9a62158a43e2cb7f971485e639dcd
kyle mcburnett <kylemcburnett@xingpeds.com>
2024-10-27 14:28:32 -0700

    add eza and alias to make it the default ls

--- setup.sh
+++ setup.sh
@@ -47,7 +47,7 @@ add_homebrew_tap() {
 
 # Function to install Homebrew packages
 install_homebrew_packages() {
-  FORMULAE=(neovim fd ripgrep fish ag stow nmap neovide lazygit zellij)
+  FORMULAE=(neovim fd ripgrep fish ag stow nmap neovide lazygit zellij eza tldr)
   CASKS=(anki nikitabobko/tap/aerospace) # neovide is a cask but installs better as a formulae
 
   print_message "Installing Homebrew formulae..."
